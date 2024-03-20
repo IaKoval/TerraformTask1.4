5e1b64ee409455e0cb66aced0cd21021bb09eb9c
Iaroslav Koval <146448189+IaKoval@users.noreply.github.com>
2024-03-20 19:04:52 +0200

    Delete Terraform_EKS_Cluster directory

--- Terraform_EKS_Cluster/1-vpc.tf
+++
@@ -1,6 +0,0 @@
-resource "aws_vpc" "eksvpc" {
-  cidr_block = "192.168.0.0/16"
-  tags = {
-    Name = "eksvpc"
-  }
-}
--- Terraform_EKS_Cluster/2-igw.tf
+++
@@ -1,7 +0,0 @@
-resource "aws_internet_gateway" "eksvpc-igw" {
-  vpc_id = aws_vpc.eksvpc.id
-
-  tags = {
-    Name = "eksvpc-igw"
-  }
-}
