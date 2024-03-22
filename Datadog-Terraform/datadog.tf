dfac953cdecc3e7d835b2abc8e56891d4f43c993
Iaroslav Koval <146448189+IaKoval@users.noreply.github.com>
2024-03-22 15:41:12 +0200

    Terraform integration with Kubernetes and Datadog.
    Terraform automating the deployment of Kubernetes resources and configuring monitoring using Datadog.

---
+++ Datadog-Terraform/datadog.tf
@@ -0,0 +1,16 @@
+resource "datadog_monitor" "image_backoff" {
+  name               = "Image Backoff"
+  type               = "metric alert"
+  query              = "avg(last_5m):sum:kubernetes.container.restart.count{reason:imagebackoff} by {pod} > 0"
+  message            = "Container is in image backoff state."
+  notify_no_data     = true
+  notify_audit       = false
+  
+  monitor_thresholds {
+    warning  = 1
+    critical = 1
+  }
+  
+  renotify_interval  = "10"
+}
+
